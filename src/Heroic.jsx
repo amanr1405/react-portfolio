775b750a5721d2a283dc2682531a47954b6045a0
Aman <81345472+amanr1405@users.noreply.github.com>
2025-07-11 17:21:40 +0530

    updated UI

--- src/Heroic.jsx
+++ src/Heroic.jsx
@@ -6,7 +6,7 @@ export default function Heroic() {
       <div className="hero bg-base min-h-[35rem]">
         <div className="hero-content text-center">
           <div className="max-w-[40rem]">
-            <h1 className="text-5xl font-bold"> Creating <span className="colorChange text-pink-500">Smart Tech </span>That Speaks Your <span className="colorChange text-pink-500">Language</span>.</h1>
+            <h1 className="text-5xl font-bold"> Creating <span className="colorChange text-pink-500">smart tech </span>that speaks your <span className="colorChange text-pink-500">language</span>.</h1>
             <p className="py-6">
             I'm <span className="colorChange text-pink-500"><strong>Aman</strong></span>, a tech enthusiast and web developer who loves turning ideas into functional, user-friendly digital experiences. 
             </p>
